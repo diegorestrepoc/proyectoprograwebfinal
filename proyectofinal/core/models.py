1ae2af1b423680147b1d05ef560f84c77a4e0f2e
typexsebaxo <sebaa.castro@duocuc.cl>
2021-07-04 21:54:41 -0400

    agregado a bbdd

--- proyectofinal/core/models.py
+++ proyectofinal/core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Marca(models.Model):
+    idMarca = models.IntegerField(primary_key=True, verbose_name='Id')
+    nombreMarca = models.CharField(max_length=50, verbose_name="Marca")
+
+    class Meta:
+        verbose_name = 'marca'
+        verbose_name_plural = 'marcas'
+        ordering = ["nombreMarca"]
+
+    def str(self):
+        return self.nombreMarca
+
+class Producto(models.Model):
+    idProducto = models.CharField(primary_key=True, max_length=10,verbose_name='Código')
+    descripcion = models.CharField(max_length=100,verbose_name='Descripción')
+    precio = models.IntegerField(verbose_name='Precio Unitario')
+    cantidad = models.IntegerField(verbose_name='Stock')
+    imagen = models.ImageField(verbose_name='Imagen',upload_to='productos',null=True,blank=True)
+    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+
+    class Meta:
+            verbose_name = 'producto'
+            verbose_name_plural = 'productos'
+            ordering = ["descripcion"]
+
+    def str(self):
+            return self.descripcion
